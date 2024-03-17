e68de7654672c84b87eb20cfee9fb97ddce69e82
Wolf McNally <wolf@wolfmcnally.com>
2024-03-17 01:50:27 -0700

    Cleanup.

--- Package.swift
+++ Package.swift
@@ -16,9 +16,9 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/WolfMcNally/WolfBase", from: "6.0.0"),
-        .package(url: "https://github.com/ChimeHQ/Flexer.git", from: "0.1.0"),
-        .package(url: "https://github.com/BlockchainCommons/BCSwiftSecureComponents.git", from: "8.0.0"),
-        .package(url: "https://github.com/BlockchainCommons/BCSwiftEnvelope.git", from: "4.0.0"),
+        .package(url: "https://github.com/ChimeHQ/Flexer", from: "0.1.0"),
+        .package(url: "https://github.com/BlockchainCommons/BCSwiftSecureComponents", from: "8.0.0"),
+        .package(url: "https://github.com/BlockchainCommons/BCSwiftEnvelope", from: "4.0.0"),
         .package(url: "https://github.com/BlockchainCommons/BCSwiftCrypto", from: "4.0.0"),
     ],
     targets: [
